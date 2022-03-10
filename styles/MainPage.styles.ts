6d912437056cedda1ad0c1fcaf28c772f70474a3
wem519 <wem519@gmail.com>
2022-03-10 23:15:07 +0900

    기본 페이지 화면 만들기

---
+++ styles/MainPage.styles.ts
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled"
+import { TextField } from "@mui/material"
+
+export const Wrapper = styled.div`
+    margin: 0 auto;
+    padding: 20px;
+`
+
+export const Title = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    font-size: 30px;
+`
+
+export const Footprints = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    margin-bottom: 10px;
+`
+export const Step = styled.div`
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+`
+export const Input = styled(TextField)`
+
+`
