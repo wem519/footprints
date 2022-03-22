84a059a75b10b0d0bf9b95f957774bcca92273d9
wem519 <wem519@gmail.com>
2022-03-22 14:32:22 +0900

    getDocs promise로 변경: data 불러오기 안됨

--- src/pages/StepList.tsx
+++ src/pages/StepList.tsx
@@ -17,14 +17,24 @@ export default function StepList() {
   const router = useRouter();
   const [daylist, setDaylist] = useState([]);
   useEffect(() => {
-    async function onLoad() {
-      const daylist = collection(getFirestore(firebaseApp), "daylist");
-      const result = await getDocs(daylist);
-      const docs = result.docs.map((el) => el.data());
-      setDaylist(docs);
-      console.log(docs);
-    }
-    onLoad();
+    // async function onLoad() {
+    //   const daylist = collection(getFirestore(firebaseApp), "daylist");
+    //   const result = await getDocs(daylist);
+    //   const docs = result.docs.map((el) => el.data());
+    //   setDaylist(docs);
+    //   console.log(docs);
+    // }
+    // onLoad();
+    const daylist = collection(getFirestore(firebaseApp), "daylist");
+    const res = new Promise(function (resolve) {
+      resolve(getDocs(daylist));
+    }).then((result) => {
+      console.log(
+        result.docs.map((el) => {
+          el.data();
+        })
+      );
+    });
   }, []);
 
   return (
@@ -34,7 +44,7 @@ export default function StepList() {
         {/* {daylist.map((el) => {
           <div>{el}</div>;
         })} */}
-        <div>{daylist[0]}</div>
+        <div></div>
         {/* <div onClick={onClickMoveToDetail}>3.9(수)</div>
         <div onClick={onClickMoveToDetail}>3.10(목)</div>
         <div onClick={onClickMoveToDetail}>3.1(금)</div> */}
